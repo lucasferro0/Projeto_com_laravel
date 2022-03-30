e1eaca8cb0723e70d88d869a08551be37fdf480e
lucasferro0 <lucasferrobrandao@hotmail.com>
2022-03-30 16:00:12 -0300

    segundo commit

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= asset('css/agenda.css')?>">
+    <link rel="stylesheet" href="{{ asset('css/agenda.css') }}">
     <title>Agenda</title>
 </head>
 <body>
@@ -18,7 +18,7 @@
 
         <ul class="list-group">
             @foreach($lista as $e)
-                <li class = "list-group-item"><?= $e; ?></li>
+                <li class = "list-group-item">{{ $e; }}</li>
             @endforeach
         </ul>
     </div>
--- resources/views/autenticacao/cadastro.blade.php
+++ resources/views/autenticacao/cadastro.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?= asset('css/cadastro.css')?>">
+  <link rel="stylesheet" href="{{ asset('css/cadastro.css') }}">
   <title>Cadastro</title>
 </head>
 <body>
@@ -13,8 +13,8 @@
   <form action="#" method="post">
     <main>
       <h1>Cadastro</h1>
-      <img id="logo_user" src="<?= asset('img/user-solid.svg')?>" alt="logo_user">
-      <img id="logo_password" src="<?= asset('img/lock-solid.svg')?>" alt="logo_password">
+      <img id="logo_user" src="{{ asset('img/user-solid.svg') }}" alt="logo_user">
+      <img id="logo_password" src="{{ asset('img/lock-solid.svg') }}" alt="logo_password">
       <input id="user" name="login" type="text" placeholder="Usuário">
       <input id="password" name="senha" type="password" placeholder="Senha">
       <button type="submit">Registrar</button>
